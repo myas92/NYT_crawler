4d1b92c645d232386568947d5a349dc1e37013d1
Yaser Ahmadi <m.y.ahmadi22@gmail.com>
2022-12-02 08:46:18 +0330

    [feature]: crawl-data-mini

--- business-logic/nyt/nyt.routes.js
+++ business-logic/nyt/nyt.routes.js
@@ -6,7 +6,7 @@ const nytController = require("./nyt.controller");
 router.get("/crawl/on-time:type", nytController.crawlMainQuestionAnswersAPI);
 router.get("/crawl/on-time", nytController.crawlMainQuestionAnswersAPI);
 router.get("/crawl/links", nytController.crawlQuestionsAnswersBasedLinksAPI);
-router.get('/crawl-data-Mini', nytController.doubleCheckDataForMini);
+router.get('/crawl-data-mini', nytController.doubleCheckDataForMini);
 router.get('/send-data-mini', nytController.sendDataToProductionForMini);
 router.get('/send-data-maxi', nytController.sendDataToProductionForMaxi);
 router.get('/:date?', nytController.getQuestionsAnswerAPI);
